d8164a16422e93d1eb49e35feaaec41ede75ab3f
giogiro <gio.94.acdc@live.it>
2024-11-06 16:46:01 +0100

    aggiunto lo scheletro di bfs, implementare in dfs_func.py

---
+++ dfs_func.py
@@ -0,0 +1,10 @@
+from collections import deque
+from node_class import Node
+import utils
+
+def dfs(matrix, start, goal):
+    
+    #TODO: da implementare
+    print("< dfs_func.dfs() >  DA IMPLEMENTARE")
+    
+    return
